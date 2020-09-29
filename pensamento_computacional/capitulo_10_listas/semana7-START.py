adea908974134f362f23db89c218e24090f0178b
Rogério Silva <rogerio.silva@ifpi.edu.br>
2020-09-29 09:49:52 -0300

    Semana 7 - Atividade Starter code

---
+++ pensamento_computacional/capitulo_10_listas/semana7-START.py
@@ -0,0 +1,52 @@
+def main():
+
+    menu = '*** Brincando com Listas ***'
+    menu += '\n 1 - Inserir Valores'
+    menu += '\n 2 - Mostrar Valor posição'
+    menu += '\n 3 - Remover do Final'
+    menu += '\n 4 - Remover do Início'
+    menu += '\n 5 - Remover de Posição Específica'
+    menu += '\n N - <Crie diversas opções com os dados> '
+    menu += '\n 0 - Sair '
+    menu += '\n\n Opção >>> '
+
+    lista = []
+
+    while True:  # Condição sempre verdadeira, só irá para em caso de break ou error
+        opcao = int(input(menu))
+
+        # Verificar operacao a fazer de acordo com a opcao
+        if opcao == 1:
+            # Listas quando passadas como argumentos e modificadas por
+            # funções não é necessário retorná-las, por a variável apontará
+            # sempre para a mesma lista.
+            # Por isso nao está ```lista = inserir_valores(lista)````
+            inserir_valores(lista)
+        elif opcao == 2:
+            mostra_valor(lista)
+        elif opcao == 0:  # sair do while
+            break
+        else:
+            print('Opção Inválida')
+
+
+def inserir_valores(lista):
+    qtd = int(input('Quantos valores desejar inserir?'))
+
+    for i in range(qtd):
+        valor = int(input('Valor: '))
+        lista.append(valor)
+
+    print('Valores inseridos com sucesso!')
+    input('<enter> to continue...')
+
+
+def mostra_valor(lista):
+    posicao = int(input('Qual posição? '))
+    print('Valor buscado:')
+    print(lista[posicao])
+
+    input('<enter> to continue...')
+
+
+main()
